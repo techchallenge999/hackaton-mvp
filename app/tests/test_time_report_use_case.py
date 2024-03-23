6707f376997b9bbfbe43e852bc162d5601d853c2
Kapizany <kapizany@gmail.com>
2024-03-23 07:23:46 -0300

    fix: fix tests

--- app/tests/test_time_report_use_case.py
+++ app/tests/test_time_report_use_case.py
@@ -9,6 +9,7 @@ load_dotenv()
 @pytest.mark.django_db
 class TestTimeReportUseCase:
     use_case = TimeReportUseCase(TimeReportRepository())
-    def test_list_usecase(self, time_report_in):
+    def test_list_usecase(self, time_report_in, time_report_out):
         use_case_list = self.use_case.list({}, {})
-        assert time_report_in in use_case_list
+        assert time_report_in.pk in [time_report.id for time_report in use_case_list]
+        assert time_report_out.pk in [time_report.id for time_report in use_case_list]
