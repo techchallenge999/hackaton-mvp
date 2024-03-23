e935c84a9e348cb70831fb14365e721012cc0d9a
henrique-pimenta <94808259+henrique-pimenta@users.noreply.github.com>
2024-03-23 11:31:01 -0300

    fix(auth): disable csrf middleware

--- app/adapters/drf/settings.py
+++ app/adapters/drf/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "app.adapters.drf.shared.middlewares.DisableCSRFMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
